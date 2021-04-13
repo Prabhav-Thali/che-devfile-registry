7c836f7f22d22ca52ce3d74566425b8aac63e658
Prabhav Thali <Prabhav.Thali1@ibm.com>
2021-04-13 11:59:20 +0530

    Multiarch base images

--- arbitrary-users-patch/build_images.sh
+++ arbitrary-users-patch/build_images.sh
@@ -37,16 +37,21 @@ while read -r line; do
   dev_container_name=$(echo "$line" | tr -s ' ' | cut -f 1 -d ' ')
   base_image_name=$(echo "$line" | tr -s ' ' | cut -f 2 -d ' ')
   base_image_digest=$(echo "$line" | tr -s ' ' | cut -f 3 -d ' ')
+  platforms_supported=$(echo "$line" | tr -s ' ' | cut -f 4 -d ' ')
   echo "Building ${NAME_FORMAT}/${dev_container_name}:${TAG} based on $base_image_name ..."
-  docker build -t "${NAME_FORMAT}/${dev_container_name}:${TAG}" --no-cache --build-arg FROM_IMAGE="$base_image_digest" "${SCRIPT_DIR}"/ | cat
   if ${PUSH_IMAGES}; then
-    echo "Pushing ${NAME_FORMAT}/${dev_container_name}:${TAG} to remote registry"
-    docker push "${NAME_FORMAT}/${dev_container_name}:${TAG}" | cat
+    docker buildx build --platform ${platforms_supported} -t "${NAME_FORMAT}/${dev_container_name}:${TAG}" --no-cache --push --build-arg FROM_IMAGE="$base_image_digest" "${SCRIPT_DIR}"/ | cat
+  else 
+    docker buildx build --platform ${platforms_supported} -t "${NAME_FORMAT}/${dev_container_name}:${TAG}" --no-cache --build-arg FROM_IMAGE="$base_image_digest" "${SCRIPT_DIR}"/ | cat
   fi
-  if ${RM_IMAGES}; then # save disk space by deleting the image we just published
-    echo "Deleting ${NAME_FORMAT}/${dev_container_name}:${TAG} from local registry"
-    docker rmi "${NAME_FORMAT}/${dev_container_name}:${TAG}"
-  fi
+  #if ${PUSH_IMAGES}; then
+    #echo "Pushing ${NAME_FORMAT}/${dev_container_name}:${TAG} to remote registry"
+    #docker push "${NAME_FORMAT}/${dev_container_name}:${TAG}" | cat
+  #fi
+  #if ${RM_IMAGES}; then # save disk space by deleting the image we just published
+    #echo "Deleting ${NAME_FORMAT}/${dev_container_name}:${TAG} from local registry"
+    #docker rmi "${NAME_FORMAT}/${dev_container_name}:${TAG}"
+  #fi
   BUILT_IMAGES="${BUILT_IMAGES}    ${NAME_FORMAT}/${dev_container_name}:${TAG}\n"
 done < "${SCRIPT_DIR}"/base_images
 
